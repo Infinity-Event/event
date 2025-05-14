6a9021f900400923ea3c7c1a35636dd780358e61
priyal1102 <21512476.dypit@dypvp.edu.in>
2025-05-14 16:17:26 +0530

    Query and Response Entity

---
+++ src/main/java/com/capgemini/event/services/ResponseService.java
@@ -0,0 +1,13 @@
+package com.capgemini.event.services;
+
+import java.util.List;
+
+import com.capgemini.event.entities.Response;
+
+public interface ResponseService {
+
+	Response createResponse(Response response);
+    Response getResponseById(Long responseId);
+    List<Response> getAllResponses();
+    void deleteResponse(Long responseId);
+}
