6e5d8dcc7819ceeda25ca5bdeaa9e64ec0d0cbdc
prerna008 <114279070+prerna008@users.noreply.github.com>
2025-05-14 18:53:44 +0530

    Update UserRepo.java

--- src/main/java/com/capgemini/event/repositories/UserRepo.java
+++ src/main/java/com/capgemini/event/repositories/UserRepo.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.capgemini.event.entities.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-//	find by email
     User findByEmail(String email);
 }
