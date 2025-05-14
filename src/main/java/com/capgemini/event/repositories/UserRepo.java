f824d9079373a872e52a22a1392564899737664c
prerna008 <114279070+prerna008@users.noreply.github.com>
2025-05-14 19:05:56 +0530

    Update and rename UserRepository.java to UserRepo.java
    Updated name

--- src/main/java/com/capgemini/event/repositories/UserRepo.java
+++ src/main/java/com/capgemini/event/repositories/UserRepo.java
@@ -3,7 +3,7 @@ package com.capgemini.event.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.capgemini.event.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
 }
