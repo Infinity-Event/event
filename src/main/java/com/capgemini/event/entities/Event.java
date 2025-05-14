3527fb45cac750d87e0e3b70bd254e58c02dcc2d
Aditya-Jambale <adityajambale@gmail.com>
2025-05-14 14:37:16 +0530

    Commit

--- src/main/java/com/capgemini/event/entities/Event.java
+++ src/main/java/com/capgemini/event/entities/Event.java
@@ -19,7 +19,7 @@ import jakarta.persistence.Table;
 @Table(name = "events")
 public class Event {
 // Event class representing an event entity in the system
-	// Fields
+	// Field
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eventId;
