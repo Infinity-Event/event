d354e62a20baf37728b91e328dbd195cdd3b8bc9
RushikeshT08 <rushikeshthange3884@gmail.com>
2025-05-14 14:57:58 +0530

    user commit

---
+++ src/main/java/com/capgemini/event/services/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.capgemini.event.services;
+
+
+import com.capgemini.event.entities.User;
+import com.capgemini.event.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User updatedUser) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+            user.setPhone(updatedUser.getPhone());
+            user.setType(updatedUser.getType());
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+    
+//    end
+
+	
+}
