7835566d842f77c89bc48e27988d5460ee452b7b
prerna008 <prernarahane8@gmail.com>
2025-05-14 23:30:44 +0530

    Registration Final

--- src/main/java/com/capgemini/event/repositories/RegistrationRepo.java
+++ src/main/java/com/capgemini/event/repositories/RegistrationRepo.java
@@ -1,23 +1,22 @@
 package com.capgemini.event.repositories;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.capgemini.event.entities.Event;
 import com.capgemini.event.entities.Registration;
 import com.capgemini.event.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-
-	Optional<Registration> findByUserAndEvent(User user, Event event);
+public interface RegistrationRepo extends JpaRepository<Registration, Long> {
 
 	List<Registration> findByUser(User user);
 
 	List<Registration> findByEvent(Event event);
 
-	long countByEvent(Event event);
+	Optional<Registration> findByUserAndEvent(User user, Event event);
+
+	boolean existsByUserAndEvent(User user, Event event);
 }
