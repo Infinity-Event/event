f2d0fb240cd9423c72d6cd396cda95155d7fd706
prerna008 <prernarahane8@gmail.com>
2025-05-16 00:00:41 +0530

    Logging query

--- src/main/java/com/capgemini/event/controllers/QueryController.java
+++ src/main/java/com/capgemini/event/controllers/QueryController.java
@@ -44,21 +44,26 @@ public class QueryController {
 	public ResponseEntity<Query> getQueryById(@PathVariable Long queryId) {
 		log.info("Received request to fetch query with ID: {}", queryId);
     		Query query = queryService.getQueryById(queryId);
+    		log.debug("Query fetched: {}", query);
     		return ResponseEntity.status(HttpStatus.OK).body(query);
 }
 	
 	@PostMapping("event/{eventId}/user/{userId}")
     public ResponseEntity<Query> createQuery(@Valid @RequestBody Query query, BindingResult bindingResult,@PathVariable Long eventId, @PathVariable Long userId) {
+		log.info("Received request to create query: {}", query);
 		if(bindingResult.hasErrors() ) {
 			throw new IllegalArgumentException(bindingResult.getFieldErrors().toString());
 		}
         Query savedQuery = queryService.createEventQuery(query, userId, eventId);
+        log.debug("Query created with ID: {}", savedQuery.getQueryId());
         return ResponseEntity.status(HttpStatus.CREATED).body(savedQuery);
     }
 	
 	@DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteQuery(@PathVariable Long id) {
+		log.info("Received request to delete query with ID: {}", id);
         queryService.deleteQuery(id);
+        log.info("Query with ID {} successfully deleted", id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
