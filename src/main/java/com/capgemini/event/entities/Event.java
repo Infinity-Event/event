97b8f05e855e51f5c383dee29f3881583ad7a69f
Aditya-Jambale <adityajambale@gmail.com>
2025-05-15 11:32:19 +0530

    Commit Event

--- src/main/java/com/capgemini/event/entities/Event.java
+++ src/main/java/com/capgemini/event/entities/Event.java
@@ -19,30 +19,31 @@ import jakarta.persistence.Table;
 @Table(name = "events")
 public class Event {
 // Event class representing an event entity in the system
-	// Field
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long eventId;
 
-    @Column(nullable = false)
+    @Column(name = "title")
     private String title;
 
-    @Lob
+    @Column(name = "description")
     private String description;
 
-    @Column(nullable = false)
+    @Column(name = "date")
     private LocalDate date;
 
-    @Column(nullable = false)
+    @Column(name = "time")
     private LocalTime time;
 
-    @Column(nullable = false)
+    @Column(name = "location")
     private String location;
 
+    @Column(name = "capacity")
     private Integer capacity;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "category")
     private Category category;
 
     @ManyToOne
