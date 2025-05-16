bc1cfd6024ce923ec81559ee63c4468b0c07a943
D2E0V0I3L <dhruv2003kumbhar@gmail.com>
2025-05-16 17:04:09 +0530

    Response Exception plus Logging updating

--- src/main/java/com/capgemini/event/controllers/ResponseController.java
+++ src/main/java/com/capgemini/event/controllers/ResponseController.java
@@ -6,24 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.capgemini.event.entities.Response;
 import com.capgemini.event.services.ResponseService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/responses")
+@Slf4j
 public class ResponseController {
 
-	private ResponseService responseService;
+	private final ResponseService responseService;
 
 	@Autowired
 	public ResponseController(ResponseService responseService) {
@@ -32,41 +28,55 @@ public class ResponseController {
 
 	@GetMapping
 	public ResponseEntity<List<Response>> getAllResponses() {
-		return ResponseEntity.status(HttpStatus.OK).body(responseService.getAllResponses());
+		log.info("Fetching all responses");
+		List<Response> responses = responseService.getAllResponses();
+		log.debug("Total responses fetched: {}", responses.size());
+		return ResponseEntity.status(HttpStatus.OK).body(responses);
 	}
 
 	@GetMapping("/{responseId}")
 	public ResponseEntity<Response> getResponseById(@PathVariable Long responseId) {
+		log.info("Fetching response by ID: {}", responseId);
 		Response response = responseService.getResponseById(responseId);
 		if (response != null) {
+			log.debug("Response found: {}", response);
 			return ResponseEntity.status(HttpStatus.OK).body(response);
 		} else {
+			log.warn("Response not found for ID: {}", responseId);
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
 	
 	@GetMapping("/query/{queryId}")
-    public ResponseEntity<Response> getResponseByQueryId(@PathVariable Long queryId) {
-        try {
-            Response response = responseService.getResponseByQueryId(queryId);
-            return ResponseEntity.ok(response);
-        } catch (RuntimeException e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	public ResponseEntity<Response> getResponseByQueryId(@PathVariable Long queryId) {
+		log.info("Fetching response by Query ID: {}", queryId);
+		try {
+			Response response = responseService.getResponseByQueryId(queryId);
+			log.debug("Response found for query {}: {}", queryId, response);
+			return ResponseEntity.ok(response);
+		} catch (RuntimeException e) {
+			log.warn("No response found for Query ID: {}", queryId);
+			return ResponseEntity.notFound().build();
+		}
+	}
 
 	@PostMapping
 	public ResponseEntity<Response> createResponse(@Valid @RequestBody Response response, BindingResult bindingResult) {
+		log.info("Creating new response: {}", response);
 		if (bindingResult.hasErrors()) {
-			throw new IllegalArgumentException("Invalid Data");
+			log.error("Validation errors while creating response: {}", bindingResult.getAllErrors());
+			throw new IllegalArgumentException(bindingResult.getFieldErrors().toString());
 		}
 		Response created = responseService.createResponse(response);
+		log.debug("Response created: {}", created);
 		return ResponseEntity.status(HttpStatus.CREATED).body(created);
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteResponse(@PathVariable Long id) {
+		log.info("Deleting response with ID: {}", id);
 		responseService.deleteResponse(id);
+		log.debug("Response deleted with ID: {}", id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
