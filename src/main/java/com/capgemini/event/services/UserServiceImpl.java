3047f97a3aed2cfb1734775a70e313de683c874a
RushikeshT08 <rushikeshthange3884@gmail.com>
2025-05-16 11:18:42 +0530

    2

--- src/main/java/com/capgemini/event/services/UserServiceImpl.java
+++ src/main/java/com/capgemini/event/services/UserServiceImpl.java
@@ -1,60 +1,31 @@
 package com.capgemini.event.services;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.capgemini.event.entities.User;
 import com.capgemini.event.exceptions.UserNotFoundException;
 import com.capgemini.event.repositories.UserRepo;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class UserServiceImpl implements UserService {
 
+
 	private final UserRepo userRepository;
 
+	@Autowired
+	public UserServiceImpl(UserRepo userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    @Autowired
-    public UserServiceImpl(UserRepo userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        log.debug("Fetching all users from the repository");
-        return userRepository.findAll();
-    }
-
-	
-
-    @Override
-    public User createUser(User user) {
-        log.debug("Saving new user to the repository");
-        return userRepository.save(user);
-    }
-   
-    @Override
-    public User updateUser(Long id, User updatedUser) {
-        log.debug("Updating user with ID: {}", id);
-        return userRepository.findById(id).map(user -> {
-            user.setName(updatedUser.getName());
-            user.setEmail(updatedUser.getEmail());
-            user.setPassword(updatedUser.getPassword());
-            user.setPhone(updatedUser.getPhone());
-            user.setType(updatedUser.getType());
-            log.debug("Saving updated user to repository: {}", user);
-            return userRepository.save(user);
-        }).orElseThrow(() -> { 
-            log.warn("User not found for update with ID: {}", id);
-            return new UserNotFoundException("User with ID " + id + " not found");
-        }); 
-    }
-
-    
+	@Override
+	public List<User> getAllUsers() {
+		log.debug("Fetching all users from the repository");
+		return userRepository.findAll();
+	}
 
 	@Override
 	public User getUserById(Long id) {
@@ -65,9 +36,28 @@ public class UserServiceImpl implements UserService {
 		});
 	}
 
-	
+	@Override
+	public User createUser(User user) {
+		log.debug("Saving new user to the repository");
+		return userRepository.save(user);
+	}
 
-
+	@Override
+	public User updateUser(Long id, User updatedUser) {
+		log.debug("Updating user with ID: {}", id);
+		return userRepository.findById(id).map(user -> {
+			user.setName(updatedUser.getName());
+			user.setEmail(updatedUser.getEmail());
+			user.setPassword(updatedUser.getPassword());
+			user.setPhone(updatedUser.getPhone());
+			user.setType(updatedUser.getType());
+			log.debug("Saving updated user to repository: {}", user);
+			return userRepository.save(user);
+		}).orElseThrow(() -> {
+			log.warn("User not found for update with ID: {}", id);
+			return new UserNotFoundException("User with ID " + id + " not found");
+		});
+	}
 
 	@Override
 	public void deleteUser(Long id) {
@@ -78,4 +68,21 @@ public class UserServiceImpl implements UserService {
 		log.debug("User with ID {} deleted from repository", id);
 	}
 
+  @Override
+    public boolean existsByEmail(String email) {
+
+    	return userRepository.existsByEmail(email);
+    }
+    
+    @Override
+    public boolean existsByName(String name) {
+    	return userRepository.existsByName(name);
+    }
+    
+    @Override
+    public User findByEmailOrName(String email, String name) {
+
+    	return userRepository.findByEmailOrName(email, name)
+				.orElseThrow(()->new UserNotFoundException("Username or Email not Found !"));
+    }
 }
