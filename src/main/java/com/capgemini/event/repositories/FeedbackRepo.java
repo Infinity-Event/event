8b53ed3a8b68a67d14567427e5d58ffe41fefa97
prerna008 <114279070+prerna008@users.noreply.github.com>
2025-05-17 13:02:17 +0530

    Update FeedbackRepo.java

--- src/main/java/com/capgemini/event/repositories/FeedbackRepo.java
+++ src/main/java/com/capgemini/event/repositories/FeedbackRepo.java
@@ -7,10 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.capgemini.event.entities.Feedback;
 public interface FeedbackRepo extends JpaRepository<Feedback, Long>{
 
-    // Find all feedbacks for a specific event (by eventId)
     List<Feedback> findByEvent_EventId(Long eventId);
 
-    // Find all feedbacks by a specific user (by userId)
     List<Feedback> findByUser_UserId(Long userId);
 
 }
